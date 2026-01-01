13b84fcbd20df1be38d7c7964dd703001c97b53b
Anna Kapitanova <totoshechka19@gmail.com>
2026-01-01 18:50:13 +0300

    Реализована ленивая загрузка для страниц

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
-import { MovieDetailsPageComponent } from './components/movie-details-page/movie-details-page.component';
-import { MediaListPageComponent } from './components/media-list-page/media-list-page.component';
 import { genresResolver } from './resolvers/genres.resolver';
 import { APP_ROUTES } from './core/constants/routes.constants';
 import { MediaType } from './core/models/media-type.enum';
@@ -10,26 +8,38 @@ export const routes: Routes = [
   // Маршруты для главной страницы с табами
   {
     path: APP_ROUTES.ALL,
-    component: MediaListPageComponent,
+    loadComponent: () =>
+      import('./components/media-list-page/media-list-page.component').then(
+        m => m.MediaListPageComponent
+      ),
     data: { mediaType: MediaType.All },
     resolve: { genres: genresResolver },
   },
   {
     path: APP_ROUTES.MOVIE,
-    component: MediaListPageComponent,
+    loadComponent: () =>
+      import('./components/media-list-page/media-list-page.component').then(
+        m => m.MediaListPageComponent
+      ),
     data: { mediaType: MediaType.Movie },
     resolve: { genres: genresResolver },
   },
   {
     path: APP_ROUTES.TV,
-    component: MediaListPageComponent,
+    loadComponent: () =>
+      import('./components/media-list-page/media-list-page.component').then(
+        m => m.MediaListPageComponent
+      ),
     data: { mediaType: MediaType.Tv },
     resolve: { genres: genresResolver },
   },
   // Маршрут для детальной страницы
   {
     path: `${APP_ROUTES.MEDIA}/:type/:id`,
-    component: MovieDetailsPageComponent,
+    loadComponent: () =>
+      import('./components/movie-details-page/movie-details-page.component').then(
+        m => m.MovieDetailsPageComponent
+      ),
     canActivate: [mediaTypeGuard],
   },
   // Редирект с корневого пути на таб "Все" с сортировкой "Лучшее" по умолчанию
