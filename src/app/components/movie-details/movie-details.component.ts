0348a8609237393e251463d19bc2e60e576fb72b
Anna Kapitanova <totoshechka19@gmail.com>
2026-01-01 21:25:21 +0300

    Устранена ошибка навигации на странице деталей

--- src/app/components/movie-details/movie-details.component.ts
+++ src/app/components/movie-details/movie-details.component.ts
@@ -17,6 +17,33 @@ export class MovieDetailsComponent implements AfterViewInit {
 
   protected readonly isVisible = signal(false);
 
+  /**
+   * Преобразует ЛЮБОЙ тип медиа из API в тип, понятный для роутера ('movie' или 'tv').
+   * Обрабатывает все известные типы из Swagger и нечувствителен к регистру.
+   */
+  public get detailsPageLink(): any[] {
+    const type = this.movie.type.toLowerCase();
+    const id = this.movie.id;
+
+    switch (type) {
+      // Все, что относится к сериалам
+      case 'tv_series':
+      case 'tv_mini_series':
+      case 'tv_special':
+        return ['/media', 'tv', id];
+
+      // Все остальное (включая MOVIE, SHORT, VIDEO и т.д.) считаем фильмами
+      case 'movie':
+      case 'tv_movie':
+      case 'short':
+      case 'video':
+      case 'video_game':
+      case 'videogame':
+      default:
+        return ['/media', 'movie', id];
+    }
+  }
+
   ngAfterViewInit(): void {
     setTimeout(() => this.isVisible.set(true), 10);
   }
